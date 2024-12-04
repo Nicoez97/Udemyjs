7652c7ca0d4906a8aa7bb4b859b7dee0285d21c4
Nicolas Gutierrez <nicolasezequielgutierrezmail.com>
2024-12-04 11:30:05 -0300

    Objetos Literales mas detalles

--- JavaScript/01-fundamentos/assets/js/objeto-literal.js
+++ JavaScript/01-fundamentos/assets/js/objeto-literal.js
@@ -1,4 +1,4 @@
-let personaje = {
+const personaje = {
     nombre: 'Tony Stark',
     codeName: 'Ironman',
     vivo: false,
@@ -28,3 +28,30 @@ console.log('Ultimo traje', personaje.trajes[ personaje.trajes.length-1]);
 
 const x= 'vivo';
 console.log('Vivo',personaje[x]);
+
+console.log('Ultima pelicula',personaje['ultima-pelicula']);
+
+// Mas detalles
+
+delete personaje.edad;
+console.log(personaje);
+
+personaje.casado=true;
+
+const entriesPares=Object.entries(personaje);
+console.log(entriesPares);
+
+//personaje=true;
+
+Object.freeze(personaje);
+
+personaje.dinero=1000000;
+personaje.casado=false;
+personaje.direccion.ubicacion='Costa Rica';
+console.log(personaje);
+
+const propiedades=Object.getOwnPropertyNames( personaje);
+const valores =Object.values(personaje);
+console.log({propiedades,valores});
+
+
