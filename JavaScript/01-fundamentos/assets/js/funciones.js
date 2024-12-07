c82a497fef6b1b36e82a8fbe91321796f094290b
Nicolas Gutierrez <nicolasezequielgutierrezmail.com>
2024-12-07 09:50:53 -0300

    pro tip funciones argumentos y desestructuracion de objetos

--- JavaScript/01-fundamentos/assets/js/funciones.js
+++ JavaScript/01-fundamentos/assets/js/funciones.js
@@ -1,6 +1,11 @@
 function saludar( nombre){
-    console.log(arguments);
-    console.log('Hola '+ nombre);
+    //console.log(arguments);
+    //console.log('Hola '+ nombre);
+    return [1,2];
+
+
+    //Esto nunca se va a ejecutar
+    console.log('Soy un codigo que despues del return');
 }
 
 const saludar2 =function(nombre) {
@@ -15,8 +20,29 @@ const saludarFlecha2= (nombre) => {
     console.log('Hola '+nombre);
 }
 
-saludar('Nicolas', 40, true, 'Costa Rica');
+ const retonoDesaludar=saludar('Nicolas', 40, true, 'Costa Rica');//1
+
 //saludar2('Nicolas');
 
-saludarFlecha();
-saludarFlecha2('Melissa');
+//saludarFlecha();
+//saludarFlecha2('Melissa');
+
+function sumar(a,b) {
+    return a +b;
+}
+
+//const sumar2= (a,b) =>{
+//    return a +b;
+//}
+const sumar2= (a,b) =>a +b;
+function getAleatorio(){
+    return Math.random();
+}
+
+//En una funcion de flecha , que no tenga llaves {}
+
+const getAleatorio2= () => Math.random;
+
+
+
+console.log(getAleatorio2());
