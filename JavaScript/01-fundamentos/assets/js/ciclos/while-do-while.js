0343d05cc566d282749da2d4248b331c5cfcf265
Nicolas Gutierrez <nicolasezequielgutierrezmail.com>
2024-12-12 10:23:06 -0300

    do while

---
+++ JavaScript/01-fundamentos/assets/js/ciclos/while-do-while.js
@@ -0,0 +1,28 @@
+
+
+const carros = ['Ford','Mazda','Honda','Toyota'];
+
+let i = 0;
+
+//while( i < carros.length  ) {
+//    console.log( carros[i]);
+//    // i= i+1
+//    i++;
+//}
+
+console.warn( 'While');
+// undefined
+// null
+// false
+
+while(carros[i]){
+    if( i === 1){
+        i++
+        continue;
+    }
+    
+    console.log(carros[i]);
+    i++;
+}
+
+console.warn('Do While');
