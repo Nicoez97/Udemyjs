217b32ae25befffc7f286ed4fbaa6d19c7c85a58
Nicolas <nicolasezequielgutierrez@gmail.com>
2025-01-04 10:56:28 -0300

    Pedir carta

--- JavaScript/02-blackjack/assets/js/juego.js
+++ JavaScript/02-blackjack/assets/js/juego.js
@@ -5,30 +5,42 @@
  * 2C= Two of Spades 
  */
 
-const { shuffle } = require("underscore");
+let deck         = [];
+const tipos      = ['C','D','H','S'];
+const especiales = ['A','J','Q','K'];
 
-let deck = [];
-const tipos = ['C','D','H','S'];
-const especiales=['A','J','Q','K'];
-
+// Esta funcion crea un nuevo deck
 const crearDeck = () => {
 
-    for ( let i = 2; i<= 10; i ++) {
-        for (let tipo of tipos){
-            deck.push( i + tipo);
+    for ( let i = 2; i <= 10; i++ ) {
+        for (let tipo of tipos) {
+            deck.push ( i + tipo);
         }
     }
 
-    for(let tipo of tipos){
+    for( let tipo of tipos ) {
         for( let esp of especiales){
-            deck.push( esp + tipo)
+            deck.push( esp + tipo);
         }
     }
 
-    console.log( deck );
+    //console.log( deck );
     deck = _.shuffle(deck);
     console.log( deck );
     return deck;
 }
 
 crearDeck();
+
+//Esta funcion me permite timar un carta
+
+const pedirCarta = () => {
+    
+    const carta = deck.pop();
+
+    console.log(deck);
+    console.log(carta); // carta debe de ser de la baraja
+    return carta;
+}
+
+pedirCarta();
