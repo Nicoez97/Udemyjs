4b4864b651fa826578f40fe4b03283ae86d3e52a
Nicolas <nicolasezequielgutierrez@gmail.com>
2025-01-27 22:32:46 -0300

    sets y gets

--- JavaScript/01-fundamentos/assets/js/classes/class.js
+++ JavaScript/01-fundamentos/assets/js/classes/class.js
@@ -5,13 +5,21 @@ class Persona {
     nombre = '' ;
     codigo = '' ;
     frase  = '' ;
+    comida = '';
 
     constructor( nombre = 'Sin nombre', codigo='Sin codigo', frase='Sin frase'){
         
         this.nombre = nombre;
         this.codigo = codigo;
         this.frase  = frase;
+    }
 
+    set setComidaFavorita (comida) {
+        this.comida = comida.toUpperCase();
+    }
+
+    get getComidaFavorita () { 
+        return `La comida favorita de ${ this.nombre} es ${ this.comida} `;
     }
 
     quienSoy(){
@@ -30,8 +38,14 @@ class Persona {
 
 const spiderman =new Persona ('Peter Parker', 'Spiderman', 'Soy tu amigable vecino Sipiderman');
 const ironman =new Persona ('Tony Starck', 'Ironman', 'Yo soy Ironman');
-console.log(spiderman);
-console.log(ironman);
+//console.log(ironman);
 
 spiderman.miFrase();
-ironman.miFrase();
+//ironman.miFrase();
+
+spiderman.setComidaFavorita = 'El pie de cereza de la tia May';
+//spiderman.comida='Duende Verde';
+
+console.log (spiderman.getComidaFavorita);
+
+console.log(spiderman);
