bc9268431ff2e745858a86c00ba06a31e28226c3
Nicolas <nicolasezequielgutierrez@gmail.com>
2025-01-28 00:00:45 -0300

    Singletone

---
+++ JavaScript/01-fundamentos/assets/js/classes/singleton.js
@@ -0,0 +1,27 @@
+
+
+class Singleton {
+
+    static instancia; // undefine
+    nombre = '';
+    
+    constructor(nombre=''){
+
+        if( !!Singleton.instancia){
+            return Singleton.instancia;
+        }
+
+        Singleton.instancia=this;
+        this.nombre=nombre;
+
+        //return this;
+    }
+
+}
+
+const instancia1 = new Singleton ('Ironman');
+const instancia2  = new Singleton('Spiderman');
+const instancia3  = new Singleton('BlackPanter');
+console.log(`Nombre la la instacia1 es: ${instancia1.nombre}`);
+console.log(`Nombre la la instacia2 es: ${instancia2.nombre}`);
+console.log(`Nombre la la instacia3 es: ${instancia3.nombre}`);
