7a6b6c81ee6b73511cea01e1cfa121e2df59948d
Nicolas <nicolasezequielgutierrez@gmail.com>
2025-01-28 00:18:07 -0300

    Multiples constructores

--- JavaScript/01-fundamentos/assets/js/classes/singleton.js
+++ JavaScript/01-fundamentos/assets/js/classes/singleton.js
@@ -5,14 +5,14 @@ class Singleton {
     static instancia; // undefine
     nombre = '';
     
-    constructor(nombre=''){
+    constructor( nombre = ''){
 
-        if( !!Singleton.instancia){
+        if( !!Singleton.instancia ){
             return Singleton.instancia;
         }
 
-        Singleton.instancia=this;
-        this.nombre=nombre;
+        Singleton.instancia = this;
+        this.nombre = nombre;
 
         //return this;
     }
@@ -21,7 +21,7 @@ class Singleton {
 
 const instancia1 = new Singleton ('Ironman');
 const instancia2  = new Singleton('Spiderman');
-const instancia3  = new Singleton('BlackPanter');
+const instancia3  = new Singleton('BlackPanther');
 console.log(`Nombre la la instacia1 es: ${instancia1.nombre}`);
 console.log(`Nombre la la instacia2 es: ${instancia2.nombre}`);
 console.log(`Nombre la la instacia3 es: ${instancia3.nombre}`);
